5b1626fd3135de6fab1a68c0d3ceebc92b42e8a9
Sami6720 <78088136+Sami6720@users.noreply.github.com>
2023-01-15 17:53:09 -0500

    feat(components): add data-grid tables to DataCard

--- src/components/DataCard.js
+++ src/components/DataCard.js
@@ -1,12 +1,74 @@
-import { Card } from '@mui/material'
+import { Card, CardContent, Typography } from '@mui/material'
 import React from 'react'
+import { DataGrid } from '@mui/x-data-grid';
+
+
+
 
 
 function DataCard(props) {
 
-    const { name } = props;
+  // the following rows and columns variables are all dummy data
+  const columns = [
+    { field: 'id', headerName: 'ID', width: 90 },
+    {
+      field: 'firstName',
+      headerName: 'First name',
+      width: 150,
+      editable: false,
+    },
+    {
+      field: 'lastName',
+      headerName: 'Last name',
+      width: 150,
+      editable: false,
+    },
+    {
+      field: 'age',
+      headerName: 'Age',
+      type: 'number',
+      width: 110,
+      editable: false,
+    },
+    {
+      field: 'fullName',
+      headerName: 'Full name',
+      description: 'This column has a value getter and is not sortable.',
+      sortable: false,
+      width: 160,
+      valueGetter: (params) =>
+        `${params.row.firstName || ''} ${params.row.lastName || ''}`,
+    },
+  ];
+  
+  const rows = [
+    { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
+    { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
+    { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
+    { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
+    { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
+    { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
+    { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
+    { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
+    { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+  ];
+  
+
+  const { name } = props;
   return (
-    <Card>{name}</Card>
+    <Card>
+      <CardContent sx={{ height: 400, width: '100%' }}>
+        <Typography sx={{ fontSize: 14 }} color="text.primary" gutterBottom>
+          {name}
+        </Typography>
+        <DataGrid
+          rows={rows}
+          columns={columns}
+          pageSize={5}
+          rowsPerPageOptions={[5]}
+        />
+      </CardContent>
+    </Card>
   )
 }
 
