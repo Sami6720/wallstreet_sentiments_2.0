7420d11259cc025df76bcd7b2a43738b38f0ef52
Sami6720 <78088136+Sami6720@users.noreply.github.com>
2023-01-15 12:09:42 -0500

    feat(navbar): setup initial navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import Grid from '@mui/material/Grid'; // Grid version 1
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
-     
+      
+      <Navbar></Navbar>
+      <Grid container spacing={2}>
+        <Grid item>
+        </Grid>
+      </Grid>
     </div>
   );
 }
