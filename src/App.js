0c72b9f3658117d43f6d09672323b11d78dcd2d5
Sami6720 <78088136+Sami6720@users.noreply.github.com>
2023-01-22 10:10:33 -0500

    fix(data_cards): fix the column headers and styling

--- src/App.js
+++ src/App.js
@@ -15,14 +15,12 @@ function App() {
           {/* The empty toolbar component makes sure that  */}
         </Grid>
         <Grid item xs={12} sm={6} md={6} lg={4}>
-          <DataCard name={"Today's Data"}></DataCard>
+          <DataCard name={"In the last five mins"}></DataCard>
         </Grid>
         <Grid item xs={12} sm={6} md={6} lg={4}>
-          <DataCard name={"Yesterday's Data"}></DataCard>
+          <DataCard name={"Yesterday's end of day"}></DataCard>
         </Grid>
-        <Grid item xs={12} lg={4}>
-          <DataCard name={"This Month's Data"}></DataCard>
-        </Grid>
+
         <Grid item xs={12} sm={12} md={12} lg={12}>
           <DataCard name={"Top voted comments"}></DataCard>
         </Grid>
--- src/components/DataCard.js
+++ src/components/DataCard.js
@@ -1,63 +1,56 @@
-import { Card, CardContent, Typography } from '@mui/material'
-import React from 'react'
-import { DataGrid } from '@mui/x-data-grid';
-
-
-
-
+import { Card, CardContent, Typography } from "@mui/material";
+import React from "react";
+import { DataGrid } from "@mui/x-data-grid";
 
 function DataCard(props) {
-
   // the following rows and columns variables are all dummy data
   const columns = [
-    { field: 'id', headerName: 'ID', width: 90 },
+    // { field: "id", headerName: "Rank", width: 10, editable: false },
     {
-      field: 'firstName',
-      headerName: 'First name',
-      width: 150,
+      field: "Stocks",
+      headerName: "Stock",
+      type: "string",
+      soratble: true,
       editable: false,
+      headerAlign: "center",
+      flex: 0.2
     },
     {
-      field: 'lastName',
-      headerName: 'Last name',
-      width: 150,
+      field: "Total_Compound",
+      headerName: "Sentiment Score",
+      type: "number",
       editable: false,
+      soratble: true,
+      headerAlign: "center",
+      flex: 0.4,
     },
     {
-      field: 'age',
-      headerName: 'Age',
-      type: 'number',
-      width: 110,
+      field: "Percentage_Change",
+      headerName: "Change",
+      type: "number",
       editable: false,
-    },
-    {
-      field: 'fullName',
-      headerName: 'Full name',
-      description: 'This column has a value getter and is not sortable.',
-      sortable: false,
-      width: 160,
-      valueGetter: (params) =>
-        `${params.row.firstName || ''} ${params.row.lastName || ''}`,
+      soratble: true,
+      headerAlign: "center",
+      flex: 0.4,
     },
   ];
-  
+
   const rows = [
-    { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-    { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-    { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-    { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-    { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-    { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-    { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-    { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-    { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+    { id: 1, lastName: "Snow", firstName: "Jon", age: 35 },
+    { id: 2, lastName: "Lannister", firstName: "Cersei", age: 42 },
+    { id: 3, lastName: "Lannister", firstName: "Jaime", age: 45 },
+    { id: 4, lastName: "Stark", firstName: "Arya", age: 16 },
+    { id: 5, lastName: "Targaryen", firstName: "Daenerys", age: null },
+    { id: 6, lastName: "Melisandre", firstName: null, age: 150 },
+    { id: 7, lastName: "Clifford", firstName: "Ferrara", age: 44 },
+    { id: 8, lastName: "Frances", firstName: "Rossini", age: 36 },
+    { id: 9, lastName: "Roxie", firstName: "Harvey", age: 65 },
   ];
-  
 
   const { name } = props;
   return (
     <Card>
-      <CardContent sx={{ height: 400, width: '100%' }}>
+      <CardContent sx={{ height: 400, width: "100%" }}>
         <Typography sx={{ fontSize: 14 }} color="text.primary" gutterBottom>
           {name}
         </Typography>
@@ -69,7 +62,7 @@ function DataCard(props) {
         />
       </CardContent>
     </Card>
-  )
+  );
 }
 
-export default DataCard
+export default DataCard;
